36c2a119011623d40a58ae15245b7796d58d13c3
direct1986 <07ky@sina.com>
2023-08-20 16:05:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -185,7 +185,7 @@ def main(collectionName):
                 
                 print('file_pathhanshiyaobu.txt4')
                 soup = BeautifulSoup(response, 'html.parser', from_encoding='utf-8')
-                print(soup)
+                print(weblist["titleslect"])
                 print('soup')
                 title = soup.select(weblist["titleslect"])[0].get_text()
                 print(title)
